35e43b4f18e86894677c07ee34ec27b5bcdb90d4
João Lucas Gomes <joaolucasgomes@Joaos-Mac-mini.lan1>
2022-07-06 13:06:21 -0400

    cylinder volume fix

--- frontend/src/SuperMath/equs/cylinderVolume.js
+++ frontend/src/SuperMath/equs/cylinderVolume.js
@@ -1,5 +1,5 @@
-const volume = (radius = 0) => {
-    return Math.PI * Math.pow(radius, 2) 
+const volume = (radius = 0, height = 0) => {
+    return Math.PI * Math.pow(radius, 2)  * height 
 }
 
 module.exports = volume
